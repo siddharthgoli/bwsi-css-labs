c460abb8d1ead26b8413ad5aef7839ce9f6bba09
siddharthgoli <141174659+siddharthgoli@users.noreply.github.com>
2026-03-29 21:29:14 -0400

    fix operation input

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -51,6 +51,23 @@ def request_sanitized_number(prompt: str) -> float:
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
+def request_sanitized_operation(prompt: str) -> float:
+    """"
+        Function to request and sanitize user input for the operation
+
+        Returns:
+            st: The sanitized operation input by the user.
+    """
+    while True:
+        try:
+            operation = input(prompt).strip().lower()
+            if operation not in ("add", "subtract", "multiply", "divide"):
+                raise ValueError
+            return operation
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            
+
 def main():
     
     print(f"===== Simple Calculator =====")
@@ -58,7 +75,7 @@ def main():
     # Ask the user for sample input    
     num1 = request_sanitized_number("Enter the first number: ")
     num2 = request_sanitized_number("Enter the second number: ")
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = request_sanitized_operation("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
